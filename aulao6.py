cea6ba41c9675d8fff6659a71afee4e5797ac405
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-01-04 19:36:25 -0300

    Create aulao6.py

---
+++ aulao6.py
@@ -0,0 +1,12 @@
+n1 = int( input('digite um numero: '))
+n2 = int(input('digite outro numero: '))
+s = n1 + n2
+
+print('soma entre {} e {} vale: {}'.format(n1, n2, s))
+# int 7 -4 0 875897687
+# float 4.5 0,0076 -15.678 7.0
+# bool thue false
+# str 'abc' '7.8' ''
+
+numero = float(input('digite um numero'))
+print(numero)
