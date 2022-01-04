456db32cbaec1070c4dcab7bc33db5545d3af25a
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-01-04 19:37:12 -0300

    Create aula07.py

---
+++ aula07.py
@@ -0,0 +1,6 @@
+nome = input('qual e seu naome? ')
+print('prazer em te connhecer {:=^20}!'.format(nome))
+
+n1 = int(input('um valor'))
+n2 = int(input('outro valor '))
+print('a some vale{}'.format(n1 + n2))
