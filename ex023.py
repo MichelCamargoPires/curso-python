ec0064de663c1bd242754147078323dcfffbf008
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-01-04 19:43:58 -0300

    Create ex023.py

---
+++ ex023.py
@@ -0,0 +1,26 @@
+#faça um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos digitos separados 
+
+#EX digite um numero 1834
+
+#undade 4
+#dezena 3 
+#sentena 9
+#milhar 1
+
+numero1 = input('dig')
+numero2 = numero1[0]
+numero3 = numero1[1]
+numero4 = numero1[2]
+numero5 = numero1[3]
+
+if numero2 < -1:
+    print(numero2)
+elif numero3 < -1:
+     print(numero3)
+elif numero4  < -1:
+    print(numero4)     
+else :
+    print('4')
+
+
+#terminar sa porra
