47e052e2cb2382a3f6d1b0c614554de0f99f294d
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-01-04 19:40:07 -0300

    Create ex001.py

---
+++ ex001.py
@@ -0,0 +1,19 @@
+nome = input ('qual o seu nome?')
+print('ola ',nome,'! prazer em ter conhescer' )
+
+dia = input ('que dia vc nasceu?')
+mes = input ('qua mes vc nasceu?')
+ano = input ('que ano vc nasceu')
+
+print('dia = ',dia)
+print('mes = ',mes)
+print('ano = ',ano)
+print('------------')
+print(dia,'/',mes,'/',ano)
+
+print('------------')
+
+pn = input ('digite um numero')
+sn = input ('digite outro numero')
+ 
+print( pn + sn )
