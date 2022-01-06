8c6f273b63abd4d54dc74a882c08d844b2d1a3af
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-01-06 20:28:31 -0300

    Create ex025.py

---
+++ ex025.py
@@ -0,0 +1,13 @@
+#crie um programa e detequi se a pesso tem "silva" no nome 
+
+nome = input('digite seu nome: ')
+
+frag = nome.split()
+acho = 'silva' in frag
+
+if acho == 'silva':
+    print('tem silva')
+else:
+    print('nao tem')
+    
+    #tenho que terminar vc bb
