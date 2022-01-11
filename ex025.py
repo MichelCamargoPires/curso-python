d599d8219c38901d4668d6207d758fbc62a45776
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-01-10 21:38:27 -0300

    Update ex025.py

--- ex025.py
+++ ex025.py
@@ -1,13 +1,6 @@
-#crie um programa e detequi se a pesso tem "silva" no nome 
+nome = input('digite seu nome')
 
-nome = input('digite seu nome: ')
+formatar = nome.lower()
+acho = 'silva' in foematar
 
-frag = nome.split()
-acho = 'silva' in frag
-
-if acho == 'silva':
-    print('tem silva')
-else:
-    print('nao tem')
-    
-    #tenho que terminar vc bb
+print(acho)
