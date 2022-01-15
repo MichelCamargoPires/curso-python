7c62b7ba2d5280c883441c1c0eca171f25bbec52
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-01-15 16:39:43 -0300

    Update ex032.py

--- ex032.py
+++ ex032.py
@@ -1,6 +1,9 @@
 #faça um programa que leia um ano qualquer e mostre se ele e  bissexto
+from datetime import date
 ano = int(input('quantos dias tem o seu ano: '))
-if ano == 365:
-    print('seu ano e normal')
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('bissexto')
 else:
-    print('seu ano e bissexto')
+    print('seu ano e bissexu')
