50fde304fc9d5bfa3d01052ed9b7cef25310a428
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-01-20 19:58:15 -0300

    Create ex043.py

---
+++ ex043.py
@@ -0,0 +1,22 @@
+#desenvolva uma ogica que leia o peso e altura de uma pessoa e calcule o imc e mostre de acordo com a tabela 
+
+# abaixo de 18.5 abaixo do peso
+
+# entre 18.5 e 25 peso ideal
+
+#25 ate 30 sobrepeso
+
+# 30 ate 40 obesidade morbida 
+
+peso = float(input('digite seu pesso: '))
+altura = float(input('digite sua altura: '))
+imc = peso / (altura * altura)
+print(imc)
+if imc <= 18.5:
+    print('vc esta abaixo do peso')
+elif imc <= 25.0:
+    print('vc esta no peso ideal')
+elif imc <= 30:
+    print('vc esta com sobrepeso')
+else:
+    print('vc esta com obesidade morbida')
