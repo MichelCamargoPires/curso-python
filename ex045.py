fe8f6b864b2fecc6eac73cb593413cd77f05ed4f
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-01-21 21:49:27 -0300

    Create ex045.py

---
+++ ex045.py
@@ -0,0 +1,24 @@
+#crie um programa que faça  computador jogar jocknpo com voce :)
+import random
+print('''[1] PEDRA
+[2] PAPEL
+[3] TESOURA''')
+palpite = int(input('digite: '))
+sorteio = random.randint(1, 3)
+if sorteio == 1 and palpite == 3:
+    re = 'computador pedra... perdeu otario!!!'
+elif sorteio == 3 and palpite == 2:
+    re = 'computador tesoura... prtdeu otario!!!'
+elif sorteio == 2 and palpite == 1:
+    re = 'computador papel... perdeu otario!!'
+elif sorteio == 3 and palpite == 1:
+    re = 'vc ganho skynet aqui NAO computador resoura'
+elif sorteio == 2 and palpite == 3:
+    re = 'vc ganho skynet aqui NAO computador papel'
+elif sorteio == 1 and palpite == 2:
+    re = 'vc ganho skynet aqui NAO computador  pedra' 
+elif sorteio == palpite:
+    re = 'vc empatou com o computadot'
+
+
+print(re)
