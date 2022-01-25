0f777a7a152eed2aef05f77527e51123346a111a
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-01-24 21:12:04 -0300

    Update ex039.py

--- ex039.py
+++ ex039.py
@@ -14,9 +14,16 @@ ano = int(input('em que ano voce nasceu?: '))
 idade = atual - ano 
 if idade < 18:
     re = 18 - idade
+    anoAlistamento = re + atual
     print('falta {}'.format(re))
+    print('seu alistamento e no ano {}'.format(anoAlistamento))
 elif idade > 18:
     re = idade - 18
     print('passou {}'.format(re))
+    anoAlistamento = atual - re
+    print('seu alistamento foi no ano {}'.format(anoAlistamento))
 else:
-    print('vai se alista')
+    print('vai se alista vc tem {}'.format(idade))
+
+
+
