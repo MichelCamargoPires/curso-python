1a5de03bd505af7ab3dbfe93efe950f8e22d10dc
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-02-02 20:45:24 -0300

    Update ex045.py

--- ex045.py
+++ ex045.py
@@ -1,24 +1,31 @@
 #crie um programa que faça  computador jogar jocknpo com voce :)
-import random
-print('''[1] PEDRA
-[2] PAPEL
-[3] TESOURA''')
-palpite = int(input('digite: '))
-sorteio = random.randint(1, 3)
-if sorteio == 1 and palpite == 3:
-    re = 'computador pedra... perdeu otario!!!'
-elif sorteio == 3 and palpite == 2:
-    re = 'computador tesoura... prtdeu otario!!!'
-elif sorteio == 2 and palpite == 1:
-    re = 'computador papel... perdeu otario!!'
-elif sorteio == 3 and palpite == 1:
-    re = 'vc ganho skynet aqui NAO computador resoura'
-elif sorteio == 2 and palpite == 3:
-    re = 'vc ganho skynet aqui NAO computador papel'
-elif sorteio == 1 and palpite == 2:
-    re = 'vc ganho skynet aqui NAO computador  pedra' 
-elif sorteio == palpite:
-    re = 'vc empatou com o computadot'
 
+#import random
+#print('''[1] PEDRA
+#[2] PAPEL
+#[3] TESOURA''')
+#palpite = int(input('digite: '))
+#sorteio = random.randint(1, 3)
+#if sorteio == 1 and palpite == 3:
+#    re = 'computador pedra... perdeu otario!!!'
+#elif sorteio == 3 and palpite == 2:
+#    re = 'computador tesoura... prtdeu otario!!!'
+#elif sorteio == 2 and palpite == 1:
+#    re = 'computador papel... perdeu otario!!'
+#elif sorteio == 3 and palpite == 1:
+#    re = 'vc ganho skynet aqui NAO computador resoura'
+#elif sorteio == 2 and palpite == 3:
+#   re = 'vc ganho skynet aqui NAO computador papel'
+#elif sorteio == 1 and palpite == 2:
+#   re = 'vc ganho skynet aqui NAO computador  pedra' 
+#elif sorteio == palpite:
+#    re = 'vc empatou com o computadot'
 
-print(re)
+from random import randint
+intns = ('pedra', 'papel', 'tesoura')
+computador = randint(0, 2)
+print('''suas opçoes:
+[0] pedra
+[1] papel
+[2] tesoura''')
+jogador = int(input('qual e a sua jogada '))
