cc94782930122b5a3997926169b4d545ea19e209
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-02-06 12:42:42 -0300

    Update ex048.py

--- ex048.py
+++ ex048.py
@@ -1,7 +1,9 @@
 #faça um programa que que caucule a some entre todos os numeros inpores que sao multiblos de tres e que se encontram no intervalo de 1 ate 500
 
+cont = 0
 soma = 0 
 for c in range(1, 501, 2):
     if c % 3 == 0:
         soma = soma + c
-print(soma)
+        cont = cont + 1
+print('o resultador de {} valore resulto no resultado {}'.format(cont, soma))
