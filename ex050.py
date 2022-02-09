dc89508931553bb906f1538ebeb00dcc40f6b3a6
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-02-08 23:55:24 -0300

    Update ex050.py

--- ex050.py
+++ ex050.py
@@ -1,3 +1,9 @@
-num = int(input('gogite o numero'))
-for c in range(1, 11):
-    print('{} x {} = {}'.format(num, c, num*c))
+#desenvolva um programa que leia 6 numeros inteiros e mostre a soma apenas daqueles que forem pares . se o valor digitado for impar desconsidere 
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input('digite {}º numero: '.format(c)))
+    if num % 2 == 0:
+        soma = soma + num
+        cont = cont + 1
+print('a soma dos  {} valore inpar somados dao {}'.format(cont, soma))
