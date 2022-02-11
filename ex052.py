7b3827d31ff74209d5b480b8ce77d7283b44506b
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-02-10 22:16:28 -0300

    Create ex052.py

---
+++ ex052.py
@@ -0,0 +1,7 @@
+# desenvolva um programa que leia o primeiro termo ea razao de uma PA no final mostrande as 10 primeiras termos dessa progressao
+
+num = int(input('digite o primeiro numero: '))
+print('-'*25)
+pa = int(input('digite a pa: '))
+for c in range (num, 10, pa ):
+    print(c)
