14e36b8cb01aa37b637f53e9a26e58cb4113e2eb
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-02-22 22:14:39 -0300

    Create ex053.py

---
+++ ex053.py
@@ -0,0 +1,11 @@
+#crie um programa que leia uma frase qualquer e mostre de ela e um PALINDROMO desconsederando espaços
+
+# arara
+
+frase = str(input('digite uma frase: ')).strip().upper()
+separar = frase.split()
+junto = ''.join(separar)
+inveso =''
+for letra in range(len(junto) -1 ,-1,-1):  
+    inveso += junto[letra]  
+print(inveso)
