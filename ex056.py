038de66be6465abdc73ce6345d12c9fa75377a49
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-03-01 20:40:36 -0300

    Update ex056.py

--- ex056.py
+++ ex056.py
@@ -7,15 +7,24 @@
 #qyabtas mulhers tem menos de 20 anos 
 somaidade = 0
 mediaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
 for p in range(1, 5):
     print('------ {}° PESSOA --------'.format(p))
     nome = str(input('nome: ')).strip()
     idade = int(input('idade: '))
     sexo = str(input('sexo [M/F]')).strip()
     somaidade += idade
-
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome 
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 += 1 
 mediaidade = somaidade / 4
-print(mediaidade)
-
-# terminar
-    
+print('a media de idade do grupo e de {} anos '.format(mediaidade))
+print('o homem mais velho tem {} anos e se chama {} anos'.format(maioridadehomem, nomevelho))    
+print('ao toto sao {} mulher com menos de 20 anos'.format(totmulher20))
