439a1f70c4ea02fa9a330f4fffdade5214458cde
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-03-15 20:18:38 -0300

    Update ex061.py

--- ex061.py
+++ ex061.py
@@ -1,12 +1,15 @@
 ''' rafaça o DESAFIO 051. lendo o primeiro termo ea razao de uma PA . mostrando 
 as 10 primeiras termos da progressao usando a estrutura while'''
 
-num = int(input('digite o primeiro numero: '))
-print('-'*25)
-pa = int(input('digite a pa: '))
-decimo = num + (10 - 1) * pa 
-for c in range (num, decimo, pa ):
-    print('{}'.format(c), end=' * ' )
-print('acabou')
+print('gerador de pa')
+print('--'*10)
+primeiro = int(input('primeiro termo: '))
+razao = int(input('razao da pa: '))
+termo = primeiro
+const = 1
+while const <= 10:
+    print('{} ->>'.format(termo),end='')
+    termo += razao
+    const += 1
+print('fim')
 
-#terminar
