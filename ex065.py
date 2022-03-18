b0cd38577af5dcd787e958ec68515dfbf0ace82b
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-03-17 22:16:53 -0300

    Create ex065.py

---
+++ ex065.py
@@ -0,0 +1,9 @@
+cont = soma = p = n = 0
+while p != 'n':
+    p = str(input('quer digirar mais um nuemro[N/S]: '))
+    n = int(input('digite um numero'))
+    cont += 1
+    soma += n
+print('soma {} contador {}'.format(soma, cont)) 
+
+   termina
