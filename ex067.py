c141d7928ade037a3af43bfd9dacdab0d0cc218f
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-03-24 20:53:34 -0300

    Update ex067.py

--- ex067.py
+++ ex067.py
@@ -1,9 +1,14 @@
+'''faça um programa que mostre a tabuada de varios numeros um de cava vez 
+para cada valor digitado pelo usuario .o programa sera o numero solicidado for negativo '''
+
 
 a = 1
 while True:
     tab = int(input('digite um numero: '))
+    if tab < 0: 
+        break
     for a in range (1, 11):
-        a += 1   
         print('{} x {} = {}'.format(tab, a, a * tab))
-        if tab < 0:
-            break
+        a += 1
+print('fim')
+        
