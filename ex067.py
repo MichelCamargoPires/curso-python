aeeab8b738dd17bcd4c870ac6445f6f93a3bea73
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-03-23 22:04:49 -0300

    Update ex067.py

--- ex067.py
+++ ex067.py
@@ -1,8 +1,9 @@
 
 a = 1
-
 while True:
     tab = int(input('digite um numero: '))
-    while a < 10:
-        a += 1
+    for a in range (1, 11):
+        a += 1   
         print('{} x {} = {}'.format(tab, a, a * tab))
+        if tab < 0:
+            break
